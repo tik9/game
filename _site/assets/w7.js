815b06ac2e4f40f36a88c43bd7c5d576495a907a
ti ko <user153015@gmail.com>
2021-02-27 10:22:38 +0100

    commit from tik9

--- _site/assets/w7.js
+++ _site/assets/w7.js
@@ -1,6 +1,6 @@
 window.onload=function(){
     
-// console.log(4)
+console.log(4)
 var myGamePiece;
 
 function startGame() {
@@ -8,47 +8,52 @@ function startGame() {
   myGamePiece = new component(10, 120,30, 30, 'red' );
 }
 
-
 myGameArea = {
-  canvas : document.getElementById("myCanvas"),
+  canvas : document.getElementById("mycanvas"),
   start : function() {
     this.canvas.width = 640;
     this.canvas.height = 320;
     this.context = this.canvas.getContext('2d');
     //this.canvas.style.cursor='none'
-    // this.interval = setInterval(updateGameArea, 100);
+     this.interval = setInterval(updateGameArea, 100);
     
-    // window.addEventListener('keydown', function (e) {
+    //window.addEventListener('keydown', function (e) {
       //myGameArea.key = e.key;
-    //  myGameArea.keys = (myGameArea.keys|| [] );
-    //   myGameArea.keys[e.key] = true;
-    // })
-    // window.addEventListener('keyup', function (e) {
-    //   myGameArea.keys[e.key] = false;
-    // })
+    //})
+    //window.addEventListener('keyup',function(e){
+      //myGameArea.key=false
+    //})
+    window.addEventListener('keydown',function(e){
+      myGameArea.keys = (myGameArea.keys|| [] );
+       myGameArea.keys[e.key] = true;
+     })
+     window.addEventListener('keyup', function (e) {
+       myGameArea.keys[e.key] = false;
+     })
     
     //  window.addEventListener('mousemove', function (e) {
     //   myGameArea.x = e.pageX;
     //   myGameArea.y = e.pageY;
     // })
   },
-  // clear : function() {
-  //   this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  // }
+   clear : function() {
+     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+   }
 }
 
 function component(x, y,width,height,color) {
   this.width = width;
   this.height = height;
+  this.speedX=0  
+  this.speedY=0
+
   this.x = x;
   this.y = y;
-  // this.update = function(){
+   this.update = function(){
     ctx = myGameArea.context;
-    ctx.fillStyle = this.color;
+    ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
-  // }
-   //this.speedX = 0;
-  //this.speedY = 0;
+   }
     
    this.newPos = function() {
     this.x += this.speedX;
@@ -56,13 +61,16 @@ function component(x, y,width,height,color) {
   }
 }
 
-// function updateGameArea() {
-//   myGameArea.clear();
-  
-//   if (myGameArea.x&&myGameArea.y){
-//       myGamePiece.x=myGameArea.x
-//       myGamePiece.y=myGameArea.y
-//     }
+ function updateGameArea() {
+   myGameArea.clear();
+   //myGamePiece.x+=1
+
+   if (myGameArea.key == 'ArrowDown'){
+       
+       myGamePiece.speedY=1
+    }
+    myGamePiece.newPos();
+
 //   if (myGameArea.keys){
 //   if (myGameArea.keys['ArrowLeft']) {
 //         myGamePiece.speedX = -1; 
@@ -79,28 +87,36 @@ function component(x, y,width,height,color) {
 //     }
 //   }    
 //   myGamePiece.newPos()
-//   myGamePiece.update()
-// }
+   myGamePiece.update()
+ }
 
-// document.getElementById('moveup').addEventListener('mousedown',function() {
-//   myGamePiece.speedY -= 1;
-// })
-// document.getElementById('moveup').addEventListener('mouseup',function() {myGamePiece.speedY =0;})
-// document.getElementById('moveup').addEventListener('touchstart',function() {
-//   myGamePiece.speedY -= 1;
-// })
+ document.getElementById('moveup').addEventListener('mouseup',function() {
+   myGamePiece.speedY = 0;
+      myGamePiece.speedX = 0;
 
-// document.getElementById('movedown').addEventListener('click',function() {
-//   myGamePiece.speedY += 1;
-// })
+ })
+ 
+ document.getElementById('moveup').addEventListener('mousedown',function() {
+   myGamePiece.speedY -= 1;
+ })
+ document.getElementById('moveup').addEventListener('touchstart',function() {
+   myGamePiece.speedY -= 1;
+ })
 
-// document.getElementById('moveleft').addEventListener('click',function() {
-//   myGamePiece.speedX -= 1;
-// })
+ document.getElementById('movedown').addEventListener('mouseup',function() {
+   myGamePiece.speedY = 0;
+ })
+ document.getElementById('movedown').addEventListener('mousedown',function() {
+   myGamePiece.speedY += 1;
+ })
 
-// document.getElementById('moveright').addEventListener('click',function() {
-//   myGamePiece.speedX += 1;
-// })
+ document.getElementById('moveleft').addEventListener('click',function() {
+   myGamePiece.speedX -= 1;
+ })
 
-startGame()
+ document.getElementById('moveright').addEventListener('click',function() {
+   myGamePiece.speedX += 1;
+ })
+
+    startGame()
 }
---
+++ w.md
@@ -0,0 +1,13 @@
+---
+layout: default
+title: Touch game
+js: w7
+css: game
+---
+
+<canvas id=mycanvas></canvas>
+
+<button id=moveup class=btn>up</button>
+<button id=movedown class=btn>down</button>
+<button id=moveleft class=btn>left</button>
+<button id=moveright class=btn>right</button>
